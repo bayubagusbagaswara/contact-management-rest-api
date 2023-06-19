7be39c961c545480317df1b8c50f977a2b3045b2
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-19 09:57:05 +0700

    Initial commit

---
+++ src/main/java/programmerzamannow/contact/ContactManagementRestfulApiApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactManagementRestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactManagementRestfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/contact/ContactManagementRestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManagementRestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

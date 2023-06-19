dedfdb393fcff97b159af9cc54aad9f660468f6a
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-19 16:56:12 +0700

    implement method register

--- src/main/java/programmerzamannow/contact/service/impl/UserServiceImpl.java
+++ src/main/java/programmerzamannow/contact/service/impl/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package programmerzamannow.contact.service.impl;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import programmerzamannow.contact.dto.RegisterUserRequest;
+import programmerzamannow.contact.entity.User;
+import programmerzamannow.contact.exception.ApiException;
 import programmerzamannow.contact.repository.UserRepository;
+import programmerzamannow.contact.security.BCrypt;
 import programmerzamannow.contact.service.UserService;
+
+import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,5 +25,26 @@ public class UserServiceImpl implements UserService {
         this.validator = validator;
     }
 
+    @Transactional
+    @Override
+    public void register(RegisterUserRequest registerUserRequest) {
+        Set<ConstraintViolation<RegisterUserRequest>> constraintViolations = validator.validate(registerUserRequest);
 
+        if (constraintViolations.size() != 0) {
+            // error
+            throw new ConstraintViolationException(constraintViolations);
+        }
+
+        if (userRepository.existsById(registerUserRequest.getUsername())) {
+            // user is already exist
+            throw new ApiException("Username already register");
+        }
+
+        User user = new User();
+        user.setUsername(registerUserRequest.getUsername());
+        user.setPassword(BCrypt.hashpw(registerUserRequest.getPassword(), BCrypt.gensalt()));
+        user.setName(registerUserRequest.getName());
+
+        userRepository.save(user);
+    }
 }

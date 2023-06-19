858047245c842e3e973c58d31e95a83694ba3674
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-19 11:28:40 +0700

    add schema table users

---
+++ src/main/resources/schema/1_table_users.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    token VARCHAR(100),
+    token_expired_at BIGINT,
+    PRIMARY KEY (username),
+    UNIQUE (token)
+);
+
+SELECT * FROM users;

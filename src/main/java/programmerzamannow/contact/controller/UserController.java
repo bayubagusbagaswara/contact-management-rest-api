e7bfc01dc4b9c1c1aa48f2a8fd8e9a0b4f2fdb38
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-19 16:59:11 +0700

    add method handler register

--- src/main/java/programmerzamannow/contact/controller/UserController.java
+++ src/main/java/programmerzamannow/contact/controller/UserController.java
@@ -1,6 +1,11 @@
 package programmerzamannow.contact.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import programmerzamannow.contact.dto.RegisterUserRequest;
+import programmerzamannow.contact.dto.WebResponse;
 import programmerzamannow.contact.service.UserService;
 
 @RestController
@@ -11,4 +16,15 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
+    @PostMapping(
+            path = "/api/users",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> register(@RequestBody RegisterUserRequest registerUserRequest) {
+        userService.register(registerUserRequest);
+        return WebResponse.<String>builder().data("OK").build();
+    }
+
 }

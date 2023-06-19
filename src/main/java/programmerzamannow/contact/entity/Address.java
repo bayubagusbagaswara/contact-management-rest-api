325d0cfd2113ba12d51cd97379e45e6006e0a9be
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-19 13:49:26 +0700

    add entity Address

---
+++ src/main/java/programmerzamannow/contact/entity/Address.java
@@ -0,0 +1,34 @@
+package programmerzamannow.contact.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+
+    @Id
+    private String id;
+
+    private String street;
+
+    private String city;
+
+    private String province;
+
+    private String country;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    private Contact contact;
+}

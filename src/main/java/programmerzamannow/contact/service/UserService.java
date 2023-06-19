3001b3e16a1fd211bcd89b3d4f1618836fd4db98
Bayu Bagus Bagaswara <bayubagusbagaswara@gmail.com>
2023-06-19 14:01:37 +0700

    add method contract register

--- src/main/java/programmerzamannow/contact/service/UserService.java
+++ src/main/java/programmerzamannow/contact/service/UserService.java
@@ -1,4 +1,8 @@
 package programmerzamannow.contact.service;
 
+import programmerzamannow.contact.dto.RegisterUserRequest;
+
 public interface UserService {
+
+    void register(RegisterUserRequest registerUserRequest);
 }

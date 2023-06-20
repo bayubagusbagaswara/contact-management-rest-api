2571f0f816f8f4e759098f9cf58d9df3bc35d703
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-20 09:48:20 +0700

    add unit test UserControllerTest

---
+++ src/test/java/programmerzamannow/contact/controller/UserControllerTest.java
@@ -0,0 +1,12 @@
+package programmerzamannow.contact.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void register() {
+    }
+}

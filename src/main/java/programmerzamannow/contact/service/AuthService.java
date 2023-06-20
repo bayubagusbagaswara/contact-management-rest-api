91ba1affff3cf2d9306e5b17bd7b3cd3a162a43f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-20 10:12:11 +0700

    add method contract login

--- src/main/java/programmerzamannow/contact/service/AuthService.java
+++ src/main/java/programmerzamannow/contact/service/AuthService.java
@@ -1,4 +1,9 @@
 package programmerzamannow.contact.service;
 
+import programmerzamannow.contact.dto.LoginUserRequest;
+import programmerzamannow.contact.dto.TokenResponse;
+
 public interface AuthService {
+
+    TokenResponse login(LoginUserRequest request);
 }

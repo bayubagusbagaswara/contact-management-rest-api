2d3ffaf07d8e48358bb02dd5f19e93b487b2ecb7
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-20 14:04:44 +0700

    add method handler login

--- src/main/java/programmerzamannow/contact/controller/AuthController.java
+++ src/main/java/programmerzamannow/contact/controller/AuthController.java
@@ -1,6 +1,12 @@
 package programmerzamannow.contact.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import programmerzamannow.contact.dto.LoginUserRequest;
+import programmerzamannow.contact.dto.TokenResponse;
+import programmerzamannow.contact.dto.WebResponse;
 import programmerzamannow.contact.service.AuthService;
 
 @RestController
@@ -11,4 +17,17 @@ public class AuthController {
     public AuthController(AuthService authService) {
         this.authService = authService;
     }
+
+    @PostMapping(
+            path = "/api/auth/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<TokenResponse> login(@RequestBody LoginUserRequest request) {
+        TokenResponse tokenResponse = authService.login(request);
+        return WebResponse.<TokenResponse>builder()
+                .data(tokenResponse)
+                .build();
+    }
+
 }

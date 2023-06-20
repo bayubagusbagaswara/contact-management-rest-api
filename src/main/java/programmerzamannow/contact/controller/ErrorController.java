33beae38d55c3f63744ac2a084cd85ae176b3924
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-20 09:43:40 +0700

    add error handler constraintViolation

--- src/main/java/programmerzamannow/contact/controller/ErrorController.java
+++ src/main/java/programmerzamannow/contact/controller/ErrorController.java
@@ -1,7 +1,20 @@
 package programmerzamannow.contact.controller;
 
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import programmerzamannow.contact.dto.WebResponse;
 
 @RestControllerAdvice
 public class ErrorController {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<WebResponse<String>> constraintViolationException(ConstraintViolationException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(WebResponse.<String>builder().errors(exception.getMessage()).build());
+    }
+
 }

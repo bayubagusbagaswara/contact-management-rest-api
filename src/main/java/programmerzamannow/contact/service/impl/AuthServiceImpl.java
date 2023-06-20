060bd80cd5a14fa44fcfda19acd6dca32ffd39f2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-20 10:13:10 +0700

    add dependency injection ValidationService

--- src/main/java/programmerzamannow/contact/service/impl/AuthServiceImpl.java
+++ src/main/java/programmerzamannow/contact/service/impl/AuthServiceImpl.java
@@ -3,13 +3,17 @@ package programmerzamannow.contact.service.impl;
 import org.springframework.stereotype.Service;
 import programmerzamannow.contact.repository.UserRepository;
 import programmerzamannow.contact.service.AuthService;
+import programmerzamannow.contact.service.ValidationService;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
     private final UserRepository userRepository;
+    private final ValidationService validationService;
 
-    public AuthServiceImpl(UserRepository userRepository) {
+    public AuthServiceImpl(UserRepository userRepository, ValidationService validationService) {
         this.userRepository = userRepository;
+        this.validationService = validationService;
     }
+
 }

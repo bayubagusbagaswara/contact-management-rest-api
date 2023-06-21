cf5bc17fc4f32062790850c2b60e501793079576
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-21 10:09:37 +0700

    implement method get

--- src/main/java/programmerzamannow/contact/service/impl/UserServiceImpl.java
+++ src/main/java/programmerzamannow/contact/service/impl/UserServiceImpl.java
@@ -1,20 +1,16 @@
 package programmerzamannow.contact.service.impl;
 
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
-import jakarta.validation.Validator;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import programmerzamannow.contact.dto.RegisterUserRequest;
+import programmerzamannow.contact.dto.UserResponse;
 import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.repository.UserRepository;
 import programmerzamannow.contact.security.BCrypt;
 import programmerzamannow.contact.service.UserService;
 import programmerzamannow.contact.service.ValidationService;
-
-import java.util.Set;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -44,5 +40,13 @@ public class UserServiceImpl implements UserService {
         user.setName(registerUserRequest.getName());
 
         userRepository.save(user);
+    }
+
+    @Override
+    public UserResponse get(User user) {
+        return UserResponse.builder()
+                .username(user.getUsername())
+                .name(user.getName())
+                .build();
     }
 }

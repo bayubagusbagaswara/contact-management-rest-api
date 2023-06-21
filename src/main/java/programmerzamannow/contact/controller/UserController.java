841925bdc934591bec182c7ab15f3e3fac822233
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-21 10:10:39 +0700

    add method handler get

--- src/main/java/programmerzamannow/contact/controller/UserController.java
+++ src/main/java/programmerzamannow/contact/controller/UserController.java
@@ -1,11 +1,14 @@
 package programmerzamannow.contact.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import programmerzamannow.contact.dto.RegisterUserRequest;
+import programmerzamannow.contact.dto.UserResponse;
 import programmerzamannow.contact.dto.WebResponse;
+import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.service.UserService;
 
 @RestController
@@ -25,6 +28,17 @@ public class UserController {
     public WebResponse<String> register(@RequestBody RegisterUserRequest registerUserRequest) {
         userService.register(registerUserRequest);
         return WebResponse.<String>builder().data("OK").build();
+    }
+
+    @GetMapping(
+            path = "/api/users/current",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<UserResponse> get(User user) {
+        UserResponse userResponse = userService.get(user);
+        return WebResponse.<UserResponse>builder()
+                .data(userResponse)
+                .build();
     }
 
 }

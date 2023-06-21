329fe43358513aaa5dc65e4477831612ed4935a3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-21 10:08:39 +0700

    add method contract get

--- src/main/java/programmerzamannow/contact/service/UserService.java
+++ src/main/java/programmerzamannow/contact/service/UserService.java
@@ -1,8 +1,12 @@
 package programmerzamannow.contact.service;
 
 import programmerzamannow.contact.dto.RegisterUserRequest;
+import programmerzamannow.contact.dto.UserResponse;
+import programmerzamannow.contact.entity.User;
 
 public interface UserService {
 
     void register(RegisterUserRequest registerUserRequest);
+
+    UserResponse get(User user);
 }

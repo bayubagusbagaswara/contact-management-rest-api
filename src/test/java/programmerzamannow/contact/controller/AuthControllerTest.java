fe669be504ad01b7024c7be422de58e5557cbe7a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-23 21:13:11 +0700

    add unit test logoutSuccess

--- src/test/java/programmerzamannow/contact/controller/AuthControllerTest.java
+++ src/test/java/programmerzamannow/contact/controller/AuthControllerTest.java
@@ -141,4 +141,35 @@ class AuthControllerTest {
         });
     }
 
+    @Test
+    void logoutSuccess() throws Exception {
+        User user = new User();
+        user.setUsername("test");
+        user.setName("Test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        user.setToken("test");
+        user.setTokenExpiredAt(System.currentTimeMillis() + 10000000000L);
+        userRepository.save(user);
+
+        mockMvc.perform(
+                delete("/api/auth/logout")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test")
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(),
+                    new TypeReference<>() {
+                    });
+
+            assertNull(response.getErrors());
+            assertEquals("OK", response.getData());
+
+            User userDb = userRepository.findById("test").orElse(null);
+            assertNotNull(userDb);
+            assertNull(userDb.getTokenExpiredAt());
+            assertNull(userDb.getToken());
+        });
+    }
+
 }

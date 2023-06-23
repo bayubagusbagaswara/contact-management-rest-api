8db250e2e37933be2d636f6e1cfdc9a528199cc0
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-23 20:26:12 +0700

    add method contract logout

--- src/main/java/programmerzamannow/contact/service/AuthService.java
+++ src/main/java/programmerzamannow/contact/service/AuthService.java
@@ -2,8 +2,11 @@ package programmerzamannow.contact.service;
 
 import programmerzamannow.contact.dto.LoginUserRequest;
 import programmerzamannow.contact.dto.TokenResponse;
+import programmerzamannow.contact.entity.User;
 
 public interface AuthService {
 
     TokenResponse login(LoginUserRequest request);
+
+    void logout(User user);
 }

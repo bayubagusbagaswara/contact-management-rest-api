545a2fb6ba999f644b315226d85180ad77044410
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-23 20:55:03 +0700

    add method handler logout

--- src/main/java/programmerzamannow/contact/controller/AuthController.java
+++ src/main/java/programmerzamannow/contact/controller/AuthController.java
@@ -1,12 +1,14 @@
 package programmerzamannow.contact.controller;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import programmerzamannow.contact.dto.LoginUserRequest;
 import programmerzamannow.contact.dto.TokenResponse;
 import programmerzamannow.contact.dto.WebResponse;
+import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.service.AuthService;
 
 @RestController
@@ -28,6 +30,15 @@ public class AuthController {
         return WebResponse.<TokenResponse>builder()
                 .data(tokenResponse)
                 .build();
+    }
+
+    @DeleteMapping(
+            path = "/api/auth/logout",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> logout(User user) {
+        authService.logout(user);
+        return WebResponse.<String>builder().data("OK").build();
     }
 
 }

bd1c2d90230edcb224161f3820ecb7564589a29d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-24 21:09:07 +0700

    add unit test ContactControllerTest

---
+++ src/test/java/programmerzamannow/contact/controller/ContactControllerTest.java
@@ -0,0 +1,48 @@
+package programmerzamannow.contact.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import programmerzamannow.contact.entity.User;
+import programmerzamannow.contact.repository.ContactRepository;
+import programmerzamannow.contact.repository.UserRepository;
+import programmerzamannow.contact.security.BCrypt;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ContactControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @BeforeEach
+    void setUp() {
+        // user must be login
+
+        contactRepository.deleteAll();
+        userRepository.deleteAll();
+
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword(BCrypt.hashpw("test", BCrypt.gensalt()));
+        user.setName("Test");
+        user.setToken("test");
+        user.setTokenExpiredAt(System.currentTimeMillis() + 1000000);
+        userRepository.save(user);
+    }
+
+    @Test
+    void create() {
+    }
+}

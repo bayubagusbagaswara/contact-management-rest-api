ff005904ea309d81a723313ebdff461d567a580d
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-24 21:03:53 +0700

    implement method create

--- src/main/java/programmerzamannow/contact/service/impl/ContactServiceImpl.java
+++ src/main/java/programmerzamannow/contact/service/impl/ContactServiceImpl.java
@@ -1,9 +1,16 @@
 package programmerzamannow.contact.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import programmerzamannow.contact.dto.ContactResponse;
+import programmerzamannow.contact.dto.CreateContactRequest;
+import programmerzamannow.contact.entity.Contact;
+import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.repository.ContactRepository;
 import programmerzamannow.contact.service.ContactService;
 import programmerzamannow.contact.service.ValidationService;
+
+import java.util.UUID;
 
 @Service
 public class ContactServiceImpl implements ContactService {
@@ -14,6 +21,30 @@ public class ContactServiceImpl implements ContactService {
     public ContactServiceImpl(ContactRepository contactRepository, ValidationService validationService) {
         this.contactRepository = contactRepository;
         this.validationService = validationService;
+    }
+
+    @Transactional
+    @Override
+    public ContactResponse create(User currentUser, CreateContactRequest request) {
+        validationService.validate(request);
+
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setFirstName(request.getFirstName());
+        contact.setLastName(request.getLastName());
+        contact.setEmail(request.getEmail());
+        contact.setPhone(request.getPhone());
+        contact.setUser(currentUser);
+
+        contactRepository.save(contact);
+
+        return ContactResponse.builder()
+                .id(contact.getId())
+                .firstName(contact.getFirstName())
+                .lastName(contact.getLastName())
+                .email(contact.getEmail())
+                .phone(contact.getPhone())
+                .build();
     }
 
 }

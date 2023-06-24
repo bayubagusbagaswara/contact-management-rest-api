140576f1ba26b676e1c0389f659da9af1ab1951f
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-24 20:59:54 +0700

    inject ValidationService

--- src/main/java/programmerzamannow/contact/service/impl/ContactServiceImpl.java
+++ src/main/java/programmerzamannow/contact/service/impl/ContactServiceImpl.java
@@ -3,13 +3,17 @@ package programmerzamannow.contact.service.impl;
 import org.springframework.stereotype.Service;
 import programmerzamannow.contact.repository.ContactRepository;
 import programmerzamannow.contact.service.ContactService;
+import programmerzamannow.contact.service.ValidationService;
 
 @Service
 public class ContactServiceImpl implements ContactService {
 
     private final ContactRepository contactRepository;
+    private final ValidationService validationService;
 
-    public ContactServiceImpl(ContactRepository contactRepository) {
+    public ContactServiceImpl(ContactRepository contactRepository, ValidationService validationService) {
         this.contactRepository = contactRepository;
+        this.validationService = validationService;
     }
+
 }

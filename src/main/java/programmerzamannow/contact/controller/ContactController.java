2fc82c15e52c48a51a33800031b73234116849c2
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-24 21:06:05 +0700

    add method handler create

--- src/main/java/programmerzamannow/contact/controller/ContactController.java
+++ src/main/java/programmerzamannow/contact/controller/ContactController.java
@@ -1,6 +1,13 @@
 package programmerzamannow.contact.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import programmerzamannow.contact.dto.ContactResponse;
+import programmerzamannow.contact.dto.CreateContactRequest;
+import programmerzamannow.contact.dto.WebResponse;
+import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.service.ContactService;
 
 @RestController
@@ -11,4 +18,17 @@ public class ContactController {
     public ContactController(ContactService contactService) {
         this.contactService = contactService;
     }
+
+    @PostMapping(
+            path = "/api/contacts",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<ContactResponse> create(User user, @RequestBody CreateContactRequest request) {
+        ContactResponse contactResponse = contactService.create(user, request);
+        return WebResponse.<ContactResponse>builder()
+                .data(contactResponse)
+                .build();
+    }
+
 }

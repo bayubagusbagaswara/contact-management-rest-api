0e691179e860012b052d245f56e7308821aaef6a
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-24 20:59:07 +0700

    add method contract create

--- src/main/java/programmerzamannow/contact/service/ContactService.java
+++ src/main/java/programmerzamannow/contact/service/ContactService.java
@@ -1,4 +1,9 @@
 package programmerzamannow.contact.service;
 
+import programmerzamannow.contact.dto.ContactResponse;
+import programmerzamannow.contact.dto.CreateContactRequest;
+
 public interface ContactService {
+
+    ContactResponse create(CreateContactRequest request);
 }

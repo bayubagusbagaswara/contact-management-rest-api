07f0c838315a54f63ed5b7f3ce2907e26d8fe691
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-25 20:14:37 +0700

    add method contract update

--- src/main/java/programmerzamannow/contact/service/ContactService.java
+++ src/main/java/programmerzamannow/contact/service/ContactService.java
@@ -2,6 +2,7 @@ package programmerzamannow.contact.service;
 
 import programmerzamannow.contact.dto.ContactResponse;
 import programmerzamannow.contact.dto.CreateContactRequest;
+import programmerzamannow.contact.dto.UpdateContactRequest;
 import programmerzamannow.contact.entity.User;
 
 public interface ContactService {
@@ -9,4 +10,6 @@ public interface ContactService {
     ContactResponse create(User currentUser, CreateContactRequest request);
 
     ContactResponse get(User user, String id);
+
+    ContactResponse update(User user, UpdateContactRequest request);
 }

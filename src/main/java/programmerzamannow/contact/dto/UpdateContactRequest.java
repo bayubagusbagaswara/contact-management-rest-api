fc0541162a7c66c1e56d2f45ffb901f6279cead9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-25 20:13:56 +0700

    add dto UpdateContactRequest

---
+++ src/main/java/programmerzamannow/contact/dto/UpdateContactRequest.java
@@ -0,0 +1,33 @@
+package programmerzamannow.contact.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateContactRequest {
+
+    @NotBlank
+    private String id;
+
+    @NotBlank
+    @Size(max = 100)
+    private String firstName;
+
+    @Size(max = 100)
+    private String lastName;
+
+    @Email
+    @Size(max = 100)
+    private String email;
+
+    @Size(max = 100)
+    private String phone;
+}

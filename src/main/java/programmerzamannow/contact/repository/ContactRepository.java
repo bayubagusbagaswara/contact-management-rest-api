a11182b3b2ab02518f0f05d988af5461f96fe328
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-25 19:36:32 +0700

    add method findFirstByUserAndId

--- src/main/java/programmerzamannow/contact/repository/ContactRepository.java
+++ src/main/java/programmerzamannow/contact/repository/ContactRepository.java
@@ -3,7 +3,12 @@ package programmerzamannow.contact.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import programmerzamannow.contact.entity.Contact;
+import programmerzamannow.contact.entity.User;
+
+import java.util.Optional;
 
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, String> {
+
+    Optional<Contact> findFirstByUserAndId(User user, String id);
 }

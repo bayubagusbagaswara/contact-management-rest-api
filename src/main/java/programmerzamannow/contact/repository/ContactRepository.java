53ca584b6c72fdb9ff1b6d7c340f9ed017e96147
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-26 10:10:41 +0700

    extends JpaSpecificationExecutor

--- src/main/java/programmerzamannow/contact/repository/ContactRepository.java
+++ src/main/java/programmerzamannow/contact/repository/ContactRepository.java
@@ -1,6 +1,7 @@
 package programmerzamannow.contact.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import programmerzamannow.contact.entity.Contact;
 import programmerzamannow.contact.entity.User;
@@ -8,7 +9,7 @@ import programmerzamannow.contact.entity.User;
 import java.util.Optional;
 
 @Repository
-public interface ContactRepository extends JpaRepository<Contact, String> {
+public interface ContactRepository extends JpaRepository<Contact, String>, JpaSpecificationExecutor<Contact> {
 
     Optional<Contact> findFirstByUserAndId(User user, String id);
 }

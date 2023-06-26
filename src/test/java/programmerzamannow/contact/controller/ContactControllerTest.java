38bfd251fa448d71ff6326c7a3474bcac7b2fa1e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-26 09:47:54 +0700

    add unit test deleteContactNotFound

--- src/test/java/programmerzamannow/contact/controller/ContactControllerTest.java
+++ src/test/java/programmerzamannow/contact/controller/ContactControllerTest.java
@@ -222,4 +222,22 @@ class ContactControllerTest {
             assertTrue(contactRepository.existsById(response.getData().getId()));
         });
     }
+
+    @Test
+    void deleteContactNotFound() throws Exception {
+        mockMvc.perform(
+                get("/api/contacts/12345")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test")
+        ).andExpectAll(
+                status().isNotFound()
+        ).andDo(result -> {
+            WebResponse<String> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<String>>() {
+            });
+
+            assertNotNull(response.getErrors());
+        });
+    }
+
 }

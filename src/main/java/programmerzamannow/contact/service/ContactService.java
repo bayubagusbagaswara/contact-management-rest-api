2727f28520f73ebb5ce5571ed04a34ea6c47991b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-26 10:09:10 +0700

    add method contract search

--- src/main/java/programmerzamannow/contact/service/ContactService.java
+++ src/main/java/programmerzamannow/contact/service/ContactService.java
@@ -1,7 +1,10 @@
 package programmerzamannow.contact.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.transaction.annotation.Transactional;
 import programmerzamannow.contact.dto.ContactResponse;
 import programmerzamannow.contact.dto.CreateContactRequest;
+import programmerzamannow.contact.dto.SearchContactRequest;
 import programmerzamannow.contact.dto.UpdateContactRequest;
 import programmerzamannow.contact.entity.User;
 
@@ -14,4 +17,6 @@ public interface ContactService {
     ContactResponse update(User user, UpdateContactRequest request);
 
     void delete(User user, String contactId);
+
+    Page<ContactResponse> search(User user, SearchContactRequest request);
 }

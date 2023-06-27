e655401298482a5991912b293c055d731a7d625b
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-27 11:00:49 +0700

    implement method create

--- src/main/java/programmerzamannow/contact/service/impl/AddressServiceImpl.java
+++ src/main/java/programmerzamannow/contact/service/impl/AddressServiceImpl.java
@@ -1,6 +1,10 @@
 package programmerzamannow.contact.service.impl;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import programmerzamannow.contact.dto.AddressResponse;
+import programmerzamannow.contact.dto.CreateAddressRequest;
+import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.repository.AddressRepository;
 import programmerzamannow.contact.service.AddressService;
 import programmerzamannow.contact.service.ValidationService;
@@ -15,5 +19,11 @@ public class AddressServiceImpl implements AddressService {
     public AddressServiceImpl(AddressRepository addressRepository, ValidationService validationService) {
         this.addressRepository = addressRepository;
         this.validationService = validationService;
+    }
+
+    @Transactional
+    @Override
+    public AddressResponse create(User user, CreateAddressRequest request) {
+        return null;
     }
 }

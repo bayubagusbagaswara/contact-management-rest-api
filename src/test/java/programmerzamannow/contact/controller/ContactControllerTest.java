5bb8f8978df9bec5faff586e356d6f4bd8dfe6d5
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-27 10:23:51 +0700

    add unit test searchUsingPhone

--- src/test/java/programmerzamannow/contact/controller/ContactControllerTest.java
+++ src/test/java/programmerzamannow/contact/controller/ContactControllerTest.java
@@ -337,4 +337,49 @@ class ContactControllerTest {
         });
     }
 
+    @Test
+    void searchUsingPhone() throws Exception {
+        User user = userRepository.findById("test").orElseThrow();
+
+        for (int i = 0; i < 100; i++) {
+            Contact contact = new Contact();
+            contact.setId(UUID.randomUUID().toString());
+            contact.setUser(user);
+            contact.setFirstName("Bayu " + i);
+            contact.setLastName("Bagaswara");
+            contact.setEmail("bayu@mail.com");
+            contact.setPhone("082211211211");
+            contactRepository.save(contact);
+        }
+
+        Contact contact = new Contact();
+        contact.setId(UUID.randomUUID().toString());
+        contact.setUser(user);
+        contact.setFirstName("Viento");
+        contact.setLastName("Bagus");
+        contact.setEmail("viento@mail.com");
+        contact.setPhone("085123456789");
+        contactRepository.save(contact);
+
+        mockMvc.perform(
+                get("/api/contacts")
+                        .queryParam("phone", "0822")
+                        .queryParam("page", "1000")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("X-API-TOKEN", "test")
+        ).andExpectAll(
+                status().isOk()
+        ).andDo(result -> {
+            WebResponse<List<ContactResponse>> response = objectMapper.readValue(result.getResponse().getContentAsString(), new TypeReference<WebResponse<List<ContactResponse>>>() {
+            });
+
+            assertNull(response.getErrors());
+            assertEquals(0, response.getData().size());
+            assertEquals(10, response.getPaging().getTotalPage());
+            assertEquals(1000, response.getPaging().getCurrentPage());
+            assertEquals(10, response.getPaging().getSize());
+        });
+    }
+
 }

2562830a796b830cd949416923c6ec6ca645467e
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-06-27 11:05:05 +0700

    add logic method create and method toAddressResponse

--- src/main/java/programmerzamannow/contact/service/impl/AddressServiceImpl.java
+++ src/main/java/programmerzamannow/contact/service/impl/AddressServiceImpl.java
@@ -1,14 +1,20 @@
 package programmerzamannow.contact.service.impl;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 import programmerzamannow.contact.dto.AddressResponse;
 import programmerzamannow.contact.dto.CreateAddressRequest;
+import programmerzamannow.contact.entity.Address;
+import programmerzamannow.contact.entity.Contact;
 import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.repository.AddressRepository;
 import programmerzamannow.contact.repository.ContactRepository;
 import programmerzamannow.contact.service.AddressService;
 import programmerzamannow.contact.service.ValidationService;
+
+import java.util.UUID;
 
 @Service
 public class AddressServiceImpl implements AddressService {
@@ -28,6 +34,33 @@ public class AddressServiceImpl implements AddressService {
     @Transactional
     @Override
     public AddressResponse create(User user, CreateAddressRequest request) {
-        return null;
+        validationService.validate(request);
+
+        Contact contact = contactRepository.findFirstByUserAndId(user, request.getContactId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact is not found"));
+
+        Address address = new Address();
+        address.setId(UUID.randomUUID().toString());
+        address.setContact(contact);
+        address.setStreet(request.getStreet());
+        address.setCity(request.getCity());
+        address.setProvince(request.getProvince());
+        address.setCountry(request.getCountry());
+        address.setPostalCode(request.getPostalCode());
+
+        addressRepository.save(address);
+
+        return toAddressResponse(address);
+    }
+
+    private AddressResponse toAddressResponse(Address address) {
+        return AddressResponse.builder()
+                .id(address.getId())
+                .street(address.getStreet())
+                .city(address.getCity())
+                .province(address.getProvince())
+                .country(address.getCountry())
+                .postalCode(address.getPostalCode())
+                .build();
     }
 }

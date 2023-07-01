55ccce9fb6dbb2633f67831888b6340538646c37
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-01 15:47:20 +0700

    add method contract get

--- src/main/java/programmerzamannow/contact/service/AddressService.java
+++ src/main/java/programmerzamannow/contact/service/AddressService.java
@@ -7,4 +7,6 @@ import programmerzamannow.contact.entity.User;
 public interface AddressService {
 
     AddressResponse create(User user, CreateAddressRequest request);
+
+    AddressResponse get(User user, String contactId, String addressId);
 }

fbacaaa98ee2bad43c406cfef8756c1800c786e1
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-01 15:32:25 +0700

    add method handler create

--- src/main/java/programmerzamannow/contact/controller/AddressController.java
+++ src/main/java/programmerzamannow/contact/controller/AddressController.java
@@ -1,6 +1,13 @@
 package programmerzamannow.contact.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import programmerzamannow.contact.dto.AddressResponse;
+import programmerzamannow.contact.dto.CreateAddressRequest;
+import programmerzamannow.contact.dto.WebResponse;
+import programmerzamannow.contact.entity.User;
 import programmerzamannow.contact.service.AddressService;
 
 @RestController
@@ -11,4 +18,19 @@ public class AddressController {
     public AddressController(AddressService addressService) {
         this.addressService = addressService;
     }
+
+    @PostMapping(
+            path = "/api/contacts/{contactId}/addresses"
+    )
+    public WebResponse<AddressResponse> create(User user,
+                                               @RequestBody CreateAddressRequest request,
+                                               @PathVariable("contactId") String contactId) {
+
+        request.setContactId(contactId);
+        AddressResponse addressResponse = addressService.create(user, request);
+        return WebResponse.<AddressResponse>builder()
+                .data(addressResponse)
+                .build();
+    }
+
 }

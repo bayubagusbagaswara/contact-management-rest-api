f3878ea201d5fc861512232871240ee5c26102d4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-01 15:35:23 +0700

    add unit testing AddressControllerTest

---
+++ src/test/java/programmerzamannow/contact/controller/AddressControllerTest.java
@@ -0,0 +1,43 @@
+package programmerzamannow.contact.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import programmerzamannow.contact.repository.AddressRepository;
+import programmerzamannow.contact.repository.ContactRepository;
+import programmerzamannow.contact.repository.UserRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AddressControllerTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        addressRepository.deleteAll();
+        contactRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+}
